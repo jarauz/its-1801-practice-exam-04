44c811039fcf1d64b27b03db5205ce586873686d
Julio Arauz <jarauz@alumni.pitt.edu>
2022-12-01 15:47:14 +0000

    Added a square function

--- main.py
+++ main.py
@@ -39,14 +39,37 @@ def triangle(turtleName, lineColor):
     turtleName.forward(50)
     turtleName.left(120)
 
+def square(turtleName, lineColor):
+  turtleName.color(lineColor)
+  for index in range(4):
+    turtleName.forward(50)
+    turtleName.left(90)
+
 casper.speed('fastest')
 
-index = 0
+casper.up()
+casper.setpos(-80,0)
+casper.down()
 
+index = 0
 for t in range(35):
-  print ('The value of index is: ' + str(index))
+  # print ('The value of index is: ' + str(index))
   triangle(casper, newColors[index])
   index = index + 1
   casper.left(10)
   if (index > 2):
     index = 0
+
+casper.up()
+casper.setpos(80,0)
+casper.down()
+
+index = 0
+for t in range(35):
+  # print ('The value of index is: ' + str(index))
+  square(casper, newColors[index])
+  index = index + 1
+  casper.left(10)
+  if (index > 2):
+    index = 0
+
